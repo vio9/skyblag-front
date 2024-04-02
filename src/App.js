c86760a0d56c67a3dbd4a5330e2beca4ce00c3aa
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-04-02 10:48:44 +0400

    hide quiz

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./Home";
 import Apropos from "./about/Apropos";
 import EnVrac from "./enVrac/EnVrac";
 import OhWow from "./ohWow/OhWow";
-import Quiz from "./quiz/Quiz";
+// import Quiz from "./quiz/Quiz";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/apropos" element={<Apropos/>}/>
         <Route path="/envrac" element={<EnVrac/>}/>
         <Route path="/ohwow" element={<OhWow/>}/>
-        <Route path="/quiz" element={<Quiz/>}/>
+        {/* <Route path="/quiz" element={<Quiz/>}/> */}
 			</Routes>
 		</BrowserRouter>
 
