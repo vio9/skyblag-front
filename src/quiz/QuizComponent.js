f133203efbddb2bd7fee536acf22e0edf510fbdb
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-04-09 11:33:26 +0400

    create quiz + quiz comp

--- src/quiz/QuizComponent.js
+++ src/quiz/QuizComponent.js
@@ -1,7 +1,15 @@
 function QuizComponent(){
     return(
         <div className="wrapper-quiz-comp">
-            <p>c'est le composant de quiz ! </p>
+            <div className="quiz-comp-wrapper">
+                <h2 className="quiz-comp-title">mettre la question ici</h2>
+                <ul>
+                    <li className="quiz-comp-answer">reponse 1</li>
+                    <li className="quiz-comp-answer">reponse 2</li>
+                    <li className="quiz-comp-answer">reponse 3</li>
+                    <li className="quiz-comp-answer">reponse 4</li>
+                </ul>
+            </div>
         </div>
     )
 }
