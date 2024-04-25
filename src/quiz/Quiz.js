bd0779fb55e212861ca46a079f3ffa3ce9ac8466
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-04-25 18:15:26 +0400

    get the answers

--- src/quiz/Quiz.js
+++ src/quiz/Quiz.js
@@ -8,13 +8,17 @@ import Footer from "../footer/Footer";
 function Quiz(){
 
     const [questionsData, setQuestionsData] = useState([]);
+    const [selectedAnswers, setSelectedAnswers] = useState({});
+    const [selectedAnswersArray, setSelectedAnswersArray] = useState([]);
+
+    let dataNumber;
 
     const getQuiz = async () => {
        try{
            const result = await axios.get("https://skyblag-back.onrender.com/api/getAllQuiz");
            setQuestionsData(result.data)
            console.log(result.data)
-      
+           dataNumber = result.data.length;
        } 
        catch(error){
            console.log(error)
@@ -25,15 +29,37 @@ function Quiz(){
        getQuiz()
    }, [])
 
+   const handleAnswerChange = (id, answerScore) => {
+    setSelectedAnswers(prevSelectedAnswers => ({
+       ...prevSelectedAnswers,
+      [id]: answerScore
+    }));
+    console.log(answerScore)
+    setSelectedAnswersArray(prevSelectedAnswersArray => {
+        const updatedSelectedAnswersArray = [...prevSelectedAnswersArray];
+        updatedSelectedAnswersArray.push(answerScore);
+        return updatedSelectedAnswersArray;
+    });
+   }
+
+   const onSubmit = () => {
+    console.log('tableau:',selectedAnswersArray)
+   }
+
     return(
         <div className="quiz-wrapper">
             <Header/>
             <h1 className="quiz-title">Découvrez votre animal totem</h1>
             {
                 questionsData.map((item, index) => (
-                    <QuizComponent key={index} {...item} />
+                    <QuizComponent key={index} 
+                        {...item} 
+                        index={index}
+                        onAnswerChange={handleAnswerChange}
+                    />
                 ))
             }
+            <button onClick={onSubmit}>envoyer</button>
             <Footer/>
         </div>
     )
--- src/quiz/QuizComponent.js
+++ src/quiz/QuizComponent.js
@@ -1,33 +1,68 @@
-
+import { useState } from "react";
 import "./quiz-component.scss";
 
 function QuizComponent({image, question, answer1, answer1Score, answer2, answer2Score,
-    answer3, answer3Score, answer4, answer4Score, id}){
-  
+    answer3, answer3Score, answer4, answer4Score, id, numeroQuestion, onAnswerChange}){
+    
+  const [selectedValue, setSelectedValue ] = useState('');
+
+  const handleRadioChange = (e) => {
+  const answerScore = e.target.value;
+  setSelectedValue(answerScore)
+  onAnswerChange(id, answerScore)
+  }
+    
     return(
         <div className="wrapper-quiz-comp">
             <div className="quiz-comp-wrapper">
                         <form className="quiz-comp-form">
                         <img className="quiz-image"src={image}/>
                         <div className='quiz-questions-wrapper'>
-                        <label key={id} className="quiz-comp-title">{question}</label>
+                        <label key={id} className="quiz-comp-title">{numeroQuestion}: {question}</label>
                         <label className="quiz-comp-label-answer">
-                            <input type="radio" name="myCheckbox" className="checkbox" value={answer1Score}/>
-                            <p className="quiz-comp-form-p">{answer1}</p>
+                            <input 
+                            type="radio" 
+                            name={`myRadioGroup_${id}`} 
+                            className="checkbox" 
+                            value={answer1Score} 
+                            checked={selectedValue === answer1Score}
+                            onChange={handleRadioChange}
+                            />
+                            <p className="quiz-comp-form-p">{answer1} , {answer1Score}</p>
                          </label>
+
                          <label className="quiz-comp-label-answer">
-                            <input type="radio" name="myCheckbox" className="checkbox" value={answer2Score}/>
-                            <p className="quiz-comp-form-p">{answer2}</p>
+                            <input type="radio" 
+                            name={`myRadioGroup_${id}`} 
+                            className="checkbox" 
+                            value={answer2Score}
+                            checked={selectedValue === answer2Score}
+                            onChange={handleRadioChange}
+                            />
+                            <p className="quiz-comp-form-p">{answer2}  {answer2Score}</p>
                          </label>
+
                          <label className="quiz-comp-label-answer">
-                            <input type="radio" name="myCheckbox" className="checkbox" value={answer3Score}/>
-                            <p className="quiz-comp-form-p">{answer3}</p>
+                            <input type="radio" 
+                            name={`myRadioGroup_${id}`} 
+                            className="checkbox" 
+                            value={answer3Score}
+                            checked={selectedValue === answer3Score}
+                            onChange={handleRadioChange}
+                            />
+                            <p className="quiz-comp-form-p">{answer3} {answer3Score}</p>
                          </label>
+
                          <label className="quiz-comp-label-answer">
-                            <input type="radio" name="myCheckbox" className="checkbox" value={answer4Score}/>
-                            <p className="quiz-comp-form-p">{answer4}</p>
+                            <input type="radio" 
+                           name={`myRadioGroup_${id}`} 
+                            className="checkbox" 
+                            value={answer4Score}
+                            checked={selectedValue === answer4Score}
+                            onChange={handleRadioChange}
+                            />
+                            <p className="quiz-comp-form-p">{answer4}, {answer4Score}</p>
                          </label>  
-                         <input type="submit" value="suivant" className="quiz-comp-submit"/>
                          </div>       
                          </form>     
             </div>
