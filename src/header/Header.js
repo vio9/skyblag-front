c6fd8225dd25349a2271f981d564cda7e4b4c0ca
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-07-15 20:34:18 +0400

    add burger menu

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import './header-style.scss';
 
 function Header({color}){
 
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
 return(
     <>
     <div className="header">
         <h1 className={`title-blog ${color}`}>Skyblag</h1>
         <hr className="title-blog-separator"/>
-        <ul className="navbar">
+        <button className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        <ul className={`navbar ${isOpen ? 'open' : ''}`}>
             <li><Link to="/" className="link_navbar">Accueil 🪴</Link></li>
             <li><Link to="/envrac" className="link_navbar">En vrac 🧺 </Link></li> 
             <li><Link to="/ohwow" className="link_navbar">Oh wow ✨</Link></li>
