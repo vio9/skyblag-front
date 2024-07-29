2b42c102c444bd1a9c84a2e50a2120a21f6d16a4
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-07-29 17:39:25 +0400

    change text about

--- src/about/AproposHome.js
+++ src/about/AproposHome.js
@@ -5,9 +5,10 @@ function AproposHome(){
     return(
         <div className="about-home-wrapper">
             <h2 className='about-home-title'>Bienvenue sur mon blog !</h2>
-            <p className='about-home-text'>Je vous propose de voyager avec moi à la Réunion, de découvrir les paysages et les endroits extraordinaires de cette île.</p>
-            <p className='about-home-text'>Ce blog est entièrement codé par mes soins (front, back, base de données et API), 
-            et vous pouvez retrouver le projet sur mon <LinkGeneric adress='https://github.com/vio9' text='profil Github'/></p>
+            <p className='about-home-text'>Vous retrouverez ici des photos de lieux et paysages de la Réunion, mais pas seulement. </p>
+            <p className='about-home-text'>Ce site est entièrement codé par mes soins (front, back, base de données et API), 
+            et vous pouvez retrouver le projet sur mon <LinkGeneric adress='https://github.com/vio9' text='profil Github'/>.</p>
+            <p className='about-home-text'>Pour plus d'infos c'est par <LinkGeneric adress='https://skyblag-sage.vercel.app/apropos' text='ici' /></p>
             <div className='about-home-wrap-img'>
                 <img className='about-home-img' src='https://i.postimg.cc/tRscCsPm/coucher-soleil-ocean.jpg' />
             </div>
