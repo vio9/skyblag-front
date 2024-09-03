4bb0f4e6512ba66ce138b9c7e61666db6061e9ba
e.violaine@yahoo.fr <e.violaine@yahoo.fr>
2024-09-03 13:25:02 +0400

    fix style error page

--- src/utils/error/Error.js
+++ src/utils/error/Error.js
@@ -7,10 +7,12 @@ function Error(){
     return(
         <div className="error-wrapper">
         <Header/>
-        <p className="error-p">oups ! il semble que la page demandée n'existe pas. Voici un joli paysage à la place</p>
+        <h1 className="error-title">Erreur 404</h1>
+        <div className="error-wrapper-p">
+        <p className="error-p">Oups ! Il semble que la page demandée n'existe pas. Voici un joli paysage à la place.</p>
         <p className="error-p">Retourner à <Link to="/" className="error-link">l'accueil 🪴</Link></p>
         <img className='error-img' src='https://i.postimg.cc/4xsJfM7Y/reunion-paysage-montagne.jpg'/>
-        
+        </div>
         <Footer/>
         </div>
     )
